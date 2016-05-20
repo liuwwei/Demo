0e1191b2e4ee20396895ffe9e1e673f9c607271c
liuwei <353291547@qq.com>
2016-05-20 13:38:14 +0800

    测试2次冲突

--- src/main/java/com/demo/Demo.java
+++ src/main/java/com/demo/Demo.java
@@ -2,7 +2,7 @@ package com.demo;
 
 public class Demo {
 	public static void main(String[] args) {
-		
+		System.out.println("qdqw");
 		System.out.println("1231");
 		System.out.println("werwew");
 		
